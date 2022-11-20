a40cb1fddd39724f5080940a3c6d49acb8b11dcd
ehmicky <ehmicky@gmail.com>
2022-11-20 17:29:49 +0100

    Add test

---
+++ test/main.js
@@ -0,0 +1,15 @@
+import test from 'ava'
+import redefineProperty from 'redefine-property'
+
+test('Handle errors when setting', (t) => {
+  // eslint-disable-next-line fp/no-proxy
+  const input = new Proxy(
+    {},
+    {
+      defineProperty() {
+        throw new Error('unsafe')
+      },
+    },
+  )
+  t.notThrows(redefineProperty.bind(undefined, input, 'prop', {}))
+})
