e3a37004d5a07448781238e4255bd323c206bea7
ehmicky <ehmicky@gmail.com>
2022-11-20 17:34:14 +0100

    Update types documentation

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,7 +1,18 @@
 /**
+ * Like
+ * [`Object.defineProperty(...)`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty)
+ * but with some additional features.
  *
  * @example
  * ```js
+ * const object = redefineProperty({}, 'prop', { value: 0, enumerable: false })
+ * console.log(Object.getOwnPropertyDescriptor(object, 'prop'))
+ * // {
+ * //   value: 0,
+ * //   enumerable: false,
+ * //   writable: true,
+ * //   configurable: true,
+ * // }
  * ```
  */
 export default function redefineProperty<T extends object>(
