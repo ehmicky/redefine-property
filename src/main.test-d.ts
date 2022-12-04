09e7a5a4c1af32cd9045d4d8f7cda689cda14351
ehmicky <ehmicky@gmail.com>
2022-12-04 20:10:42 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,30 +1,40 @@
 import redefineProperty from 'redefine-property'
-import { expectType, expectError } from 'tsd'
+import { expectType } from 'tsd'
 
 expectType<{}>(redefineProperty({}, 'prop', {}))
 expectType<{ a: 1 }>(redefineProperty({ a: 1 as const }, 'prop', {}))
 
-expectError(redefineProperty({}, 'prop', {}, true))
+// @ts-expect-error
+redefineProperty({}, 'prop', {}, true)
 
-expectError(redefineProperty(true, 'prop', {}))
+// @ts-expect-error
+redefineProperty(true, 'prop', {})
 
 redefineProperty({}, Symbol(), {})
 redefineProperty({}, 0, {})
-expectError(redefineProperty({}, true, {}))
+// @ts-expect-error
+redefineProperty({}, true, {})
 
-expectError(redefineProperty({}, 'prop', true))
+// @ts-expect-error
+redefineProperty({}, 'prop', true)
 redefineProperty({}, 'prop', { enumerable: true })
-expectError(redefineProperty({}, 'prop', { enumerable: 'true' }))
+// @ts-expect-error
+redefineProperty({}, 'prop', { enumerable: 'true' })
 redefineProperty({}, 'prop', { writable: true })
-expectError(redefineProperty({}, 'prop', { writable: 'true' }))
+// @ts-expect-error
+redefineProperty({}, 'prop', { writable: 'true' })
 redefineProperty({}, 'prop', { configurable: true })
-expectError(redefineProperty({}, 'prop', { configurable: 'true' }))
+// @ts-expect-error
+redefineProperty({}, 'prop', { configurable: 'true' })
 redefineProperty({}, 'prop', { value: true })
 redefineProperty({}, 'prop', { value: undefined })
 redefineProperty({}, 'prop', { get() {} })
-expectError(redefineProperty({}, 'prop', { get: true }))
-expectError(redefineProperty({}, 'prop', { get(value: true) {} }))
+// @ts-expect-error
+redefineProperty({}, 'prop', { get: true })
+// @ts-expect-error
+redefineProperty({}, 'prop', { get(value: true) {} })
 redefineProperty({}, 'prop', { set(value: unknown) {} })
 redefineProperty({}, 'prop', { set() {} })
 redefineProperty({}, 'prop', { set: (value: unknown) => true })
-expectError(redefineProperty({}, 'prop', { set: true }))
+// @ts-expect-error
+redefineProperty({}, 'prop', { set: true })
